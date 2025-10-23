e47a069f1dee023eda11ce6469b846d5660d879b
Diego Ferrer <zS24016706@estudiantes.uv.mx>
2025-10-23 14:49:21 -0600

    Primer commit

---
+++ Monedero.java
@@ -0,0 +1,18 @@
+public class Monedero{
+    public boolean esPosibleSuministrar(int tipo){
+        return false;
+    }
+    public void suministrar(int cantidad){
+
+    }
+
+    public void devolverCantidadIntroducida(){
+
+    }
+
+    public int cantidadIntroducida(){
+
+    }
+
+    
+}
